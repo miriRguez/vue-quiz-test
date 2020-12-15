bca4701a0c33e0137ee9abbbf58f081862540834
Miriam <miriam.r@incfile.com>
2020-12-15 17:49:04 -0600

    Start project with vue

---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="question-box-container">
+    <b-jumbotron>
+        <template slot="lead">
+            Some question
+        </template>
+        <p>For more information visit website</p>
+        <b-button variant="primary" href="#">More Info</b-button>
+    </b-jumbotron>
+    </div>
+</template>
